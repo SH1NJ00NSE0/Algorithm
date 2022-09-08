d6018328d562989a37b716ac7bfcebd4a60181ae
baeian <ddoory1103@gmail.com>
2022-09-08 14:30:39 +0900

    알파벳 빈도

---
+++ alphabet_frequency.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   int i, j = 0;
+   char s[1000001];
+   char a[27]; //알파벳 중복제거한 배열
+   int b[27]; //알파벳 개수 배열
+   scanf("%s", s);
+   while (s[i] != '\0')
+   {
+      while (a[j]!='\0')
+      {
+         if (s[i]=a[j])
+         {
+            
+         }
+         
+      }
+
+      i++;
+   }
+
+   return 0;
+}
